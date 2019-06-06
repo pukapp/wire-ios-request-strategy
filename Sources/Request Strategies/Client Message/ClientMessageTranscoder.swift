dbf6927fa8e0e9b276d2e38f9d4775b9f62fcdeb
wangjie <17054214@qq.com>
2019-06-06 19:33:01 +0800

    禁言加入时长逻辑

--- Sources/Request Strategies/Client Message/ClientMessageTranscoder.swift
+++ Sources/Request Strategies/Client Message/ClientMessageTranscoder.swift
@@ -203,8 +203,9 @@ extension ClientMessageTranscoder {
            let code = payload["code"] as? Int,
            let data = payload["data"] as? [String: Any],
            let blockTime = data["block_time"] as? Int64,
+           let duration = data["block_duration"] as? Int64,
            [1015,1014].contains(code) {
-            UserDisableSendMsgStatus.update(managedObjectContext: self.managedObjectContext, block_time: NSNumber(value: blockTime), user: message.sender?.remoteIdentifier.transportString(), conversation: message.conversation?.remoteIdentifier?.transportString(), fromPushChannel: true)
+            UserDisableSendMsgStatus.update(managedObjectContext: self.managedObjectContext, block_time: NSNumber(value: blockTime), block_duration: NSNumber(value: duration), user: message.sender?.remoteIdentifier.transportString(), conversation: message.conversation?.remoteIdentifier?.transportString(), fromPushChannel: true)
             return
         }
         
