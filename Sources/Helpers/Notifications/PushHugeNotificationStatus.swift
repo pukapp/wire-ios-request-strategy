21312d1163e1a9a16a41cb97da574f73981c65d8
wangjie <17054214@qq.com>
2021-01-20 17:30:53 +0800

    去掉万人群拉取单个id 方法

--- Sources/Helpers/Notifications/PushHugeNotificationStatus.swift
+++ Sources/Helpers/Notifications/PushHugeNotificationStatus.swift
@@ -42,47 +42,47 @@ open class PushHugeNotificationStatus: NSObject, BackgroundNotificationFetchStat
     ///
     /// - parameter eventId: UUID of the event to fetch
     /// - parameter completionHandler: The completion handler will be run when event has been downloaded and when there's no more events to fetch
-    @objc(fetchEventId:completionHandler:)
-    public func fetch(eventId: UUID, completionHandler: @escaping () -> Void) {
-        guard eventId.isType1UUID else {
-            return zmLog.error("Attempt to fetch event id not conforming to UUID type1: \(eventId)")
-        }
-        
-        if lastEventIdIsNewerThan(lastEventId: managedObjectContext.zm_lastHugeNotificationID, eventId: eventId){
-            // We have already fetched the event and will therefore immediately call the completion handler
-            Logging.eventProcessing.info("Already fetched event with [\(eventId)]")
-            return completionHandler()
-        }
-        
-        Logging.eventProcessing.info("Scheduling to fetch events notified by push [\(eventId)]")
-        
-        eventIdRanking.add(eventId)
-        completionHandlers[eventId] = completionHandler
-        
-        RequestAvailableNotification.notifyNewRequestsAvailable(nil)
-    }
+//    @objc(fetchEventId:completionHandler:)
+//    public func fetch(eventId: UUID, completionHandler: @escaping () -> Void) {
+//        guard eventId.isType1UUID else {
+//            return zmLog.error("Attempt to fetch event id not conforming to UUID type1: \(eventId)")
+//        }
+//
+//        if lastEventIdIsNewerThan(lastEventId: managedObjectContext.zm_lastHugeNotificationID, eventId: eventId){
+//            // We have already fetched the event and will therefore immediately call the completion handler
+//            Logging.eventProcessing.info("Already fetched event with [\(eventId)]")
+//            return completionHandler()
+//        }
+//
+//        Logging.eventProcessing.info("Scheduling to fetch events notified by push [\(eventId)]")
+//
+//        eventIdRanking.add(eventId)
+//        completionHandlers[eventId] = completionHandler
+//
+//        RequestAvailableNotification.notifyNewRequestsAvailable(nil)
+//    }
     
     /// Report events that has successfully been downloaded from the notification stream
     ///
     /// - parameter eventIds: List of UUIDs for events that been downloaded
     /// - parameter finished: True when when all available events have been downloaded
-    @objc(didFetchEventIds:lastEventId:finished:)
-    public func didFetch(eventIds: [UUID], lastEventId: UUID?, finished: Bool) {
-        let highestRankingEventId = eventIdRanking.firstObject as? UUID
-        
-        highestRankingEventId.apply(eventIdRanking.remove)
-        eventIdRanking.minusSet(Set<UUID>(eventIds))
-        
-        guard finished else { return }
-        
-        Logging.eventProcessing.info("Finished to fetching all available events")
-        
-        // We take all events that are older than or equal to lastEventId and add highest ranking event ID
-        for eventId in completionHandlers.keys.filter({  self.lastEventIdIsNewerThan(lastEventId: lastEventId, eventId: $0) || highestRankingEventId == $0 }) {
-            let completionHandler = completionHandlers.removeValue(forKey: eventId)
-            completionHandler?()
-        }
-    }
+//    @objc(didFetchEventIds:lastEventId:finished:)
+//    public func didFetch(eventIds: [UUID], lastEventId: UUID?, finished: Bool) {
+//        let highestRankingEventId = eventIdRanking.firstObject as? UUID
+//
+//        highestRankingEventId.apply(eventIdRanking.remove)
+//        eventIdRanking.minusSet(Set<UUID>(eventIds))
+//
+//        guard finished else { return }
+//
+//        Logging.eventProcessing.info("Finished to fetching all available events")
+//
+//        // We take all events that are older than or equal to lastEventId and add highest ranking event ID
+//        for eventId in completionHandlers.keys.filter({  self.lastEventIdIsNewerThan(lastEventId: lastEventId, eventId: $0) || highestRankingEventId == $0 }) {
+//            let completionHandler = completionHandlers.removeValue(forKey: eventId)
+//            completionHandler?()
+//        }
+//    }
     
     /// Report that events couldn't be fetched due to a permanent error
     public func didFailToFetchEvents() {
